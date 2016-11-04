1c2657cfefb713536d5f4e71ec8edeed9de0cbac
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2016-11-04 10:10:28 -0700

    create variables for each radio group Ex:Size,Sauce,Toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 // Front End Logic
 $(document).ready(function()  {
-  $("button#")
-})
+  $("button#placeOrder").submit(function(event) {
+
+    var newSize = $('input:radio[name="size"]:checked').val();
+    var newSauce = $('input:radio[name="sauce"]:checked').val();
+    var newToppings = toppingAssembly();
+    var thisSize = new Size(newSize);
+    var thisSauce = new Sauce(newSauce);
+    var thisToppings = new Toppings(newToppings);
+  });
+});
